0025cd4d464a686c7f0418a9bab0c8424988f912
Cajeff1 <149740647+Cajeff1@users.noreply.github.com>
2024-03-24 23:45:43 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Stopwatch from './stopwatch.jsx';
+
+function App() {
+ 
+  return(
+    <>
+  <Stopwatch></Stopwatch>
+  <Stopwatch></Stopwatch>
+  <Stopwatch></Stopwatch>
+  </>
+  )
+}
+
+export default App
